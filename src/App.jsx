f813fb0dfe8a44e106ade41c49ae29505c71027c
ssantiago90 <salkin.santiago@gmail.com>
2023-04-25 21:06:44 -0300

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function authUser() {
+    return {
+      username: "Matias",
+      colorPreferido: "lightblue",
+    };
+  }
+
+  function contadorDeVistas() {
+    return 123808053;
+  }
+
+  const user = authUser();
+
+  const stylesViewCount = {
+    backgroundColor: user.colorPreferido,
+    color: "black",
+    fontWeight: "700",
+    marginTop: "5",
+  };
 
   return (
-    <>
+    <div className="header">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div>
+        <h1>Hola {user.username} bienvenida a Coderhouse</h1>
+      </div>
+      <div>
+        <h2>Hola de nuevo</h2>
+        <div style={stylesViewCount}>{contadorDeVistas()}</div>
+        <div>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
