a792fa6da1ac3242b026e07a65c41c245dd198d8
ssantiago90 <salkin.santiago@gmail.com>
2023-06-06 19:59:49 -0300

    install firebase

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,23 @@ import NavBar from "./components/NavBar/NavBar";
 import Homepage from "./pages/Homepage";
 import CartView from "./components/CartView/CartView";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhzrq6NZMxQk8EhbcDNNgfUDd2XV8Whfg",
+  authDomain: "react-coder-deede.firebaseapp.com",
+  projectId: "react-coder-deede",
+  storageBucket: "react-coder-deede.appspot.com",
+  messagingSenderId: "214234342632",
+  appId: "1:214234342632:web:88d25164959139735c155d",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+// conectarnos a Firestore
+const db = getFirestore(firebaseApp);
+console.log(db);
+
 function App() {
   return (
     <CartContextProvider>
